bf177126f0e9d95a3008905569b9b09523dd7faa
borhan <itsme.bori.shariati@gmail.com>
2024-01-26 22:41:20 +0330

    tamrin1 branch

--- tamrin0/tike1.py
+++
@@ -1,2 +0,0 @@
-#this code get cleaned
-print('Hello Python')
---
+++ tamrin1/soal1-test.py
@@ -0,0 +1,12 @@
+#this code get cleaned
+checknumber=int(input())
+test=False
+shomarande=0
+for j in range(checknumber-2):
+    if (checknumber%(j+2))==0:
+        test=True
+        print('yes')
+        break
+if test==False:
+    shomarande+=1
+print(shomarande)            
